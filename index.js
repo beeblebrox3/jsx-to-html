8865d34a8aef0a7f00111ddce3c8b86e324d4ceb
Luís Henrique Faria <luish.faria@gmail.com>
2016-10-10 19:58:45 -0300

    pretty print file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server");
-require('node-jsx').install();
+var html = require("html");
+require("node-jsx").install();
 
 var Div = require("./Div.jsx");
 
 var factory = React.createFactory(Div);
 
-console.log(ReactDOMServer.renderToString(factory({})));
+console.log(
+    html.prettyPrint(ReactDOMServer.renderToString(factory({})), {ident_size: 4})
+);
